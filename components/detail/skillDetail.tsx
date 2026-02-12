55369078d047b4ebcb97ab7d00621402833d8d95
sjhong98 <sjhong98@users.noreply.github.com>
2026-02-12 21:35:18 +0900

    feat

--- components/detail/skillDetail.tsx
+++ components/detail/skillDetail.tsx
@@ -53,20 +53,20 @@ export default function SkillDetail() {
 
             <h1 className='text-4xl font-black text-sub-light ml-'>Skills</h1>
 
-            <div className='absolute top-0 right-0 flex flex-col gap-1 items-end'>
+            <div className='sm:sticky absolute mt-[-160px] top-0 right-0 flex flex-col gap-1 items-end'>
                 <div className='flex flex-row gap-2 items-center'>
-                    <div className='w-2 h-2 rounded-full' style={{ backgroundColor: '#294a02' }} />
-                    <p className='text-[12px] text-sub-light font-light w-12'>Level 1</p>
+                    <div className='w-3 h-3 rounded-full' style={{ backgroundColor: '#294a02' }} />
+                    <p className='text-[14px] text-sub-light font-light w-12'>Level 1</p>
                 </div>
                 <div className='flex flex-row gap-2 items-center'>
-                    <div className='w-2 h-2 rounded-full' style={{ backgroundColor: '#539404' }} />
-                    <p className='text-[12px] text-sub-light font-light w-12'>Level 2</p>
+                    <div className='w-3 h-3 rounded-full' style={{ backgroundColor: '#539404' }} />
+                    <p className='text-[14px] text-sub-light font-light w-12'>Level 2</p>
                 </div>
                 <div className='flex flex-row gap-2 items-center'>
-                    <div className='w-2 h-2 rounded-full' style={{ backgroundColor: '#a1f939' }} />
-                    <p className='text-[12px] text-sub-light font-light w-12'>Level 3</p>
+                    <div className='w-3 h-3 rounded-full' style={{ backgroundColor: '#a1f939' }} />
+                    <p className='text-[14px] text-sub-light font-light w-12'>Level 3</p>
                 </div>
-                <p className='text-[10px] text-sub font-light whitespace-pre-wrap leading-[14px] text-right'>{`*자세한 내용은 스킬을 클릭하여\n확인할 수 있습니다.`}</p>
+                <p className='mt-1 text-[12px] text-sub font-light whitespace-pre-wrap leading-[14px] text-right'>{`*자세한 내용은 스킬을 클릭하여\n확인할 수 있습니다.`}</p>
             </div>
 
             {/* Skills by Category */}
